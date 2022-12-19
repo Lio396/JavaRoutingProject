d0c537dd8c6b9152665a5236a5bfcf4720cf2b32
liobag <bagnoud.lionel@lernende.bfo-vs.ch>
2022-12-19 14:46:58 +0100

    bin dumm

---
+++ src/JavaPacket.java
@@ -0,0 +1,22 @@
+public class JavaPacket implements Packet{
+
+    @Override
+    public String getSrc() {
+        return null;
+    }
+
+    @Override
+    public String getDst() {
+        return null;
+    }
+
+    @Override
+    public int getSport() {
+        return 0;
+    }
+
+    @Override
+    public int getDPort() {
+        return 0;
+    }
+}
---
+++ src/JavaRouting.java
@@ -0,0 +1,6 @@
+public class JavaRouting {
+    public static void main (String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
