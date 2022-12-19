cb88686d2b862543c36eedc7ca1ede42bf974934
liobag <bagnoud.lionel@lernende.bfo-vs.ch>
2022-12-19 15:20:37 +0100

    modifiedJavaRouting

--- src/JavaRouting.java
+++ src/JavaRouting.java
@@ -1,6 +1,26 @@
-public class JavaRouting {
+public class JavaRouting implements RoutingEntry {
     public static void main (String[] args) {
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public String getMask() {
+        return null;
+    }
+
+    @Override
+    public String getGW() {
+        return null;
+    }
+
+    @Override
+    public String getCidr() {
+        return null;
     }
 }
 
